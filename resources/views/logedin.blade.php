ba0e2a584e7226e0b54cb78c225f03f0df585e7e
Nier Su <sonor.fz@163.com>
2017-09-09 22:35:24 +1000

    took care of return view logedin bug

---
+++ resources/views/logedin.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="top-first-padding"></div>
+<div class="container" style="margin-bottom: 100px">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    You are logged in!<br>
+                    Try top right corner dropdown menu for more options.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
